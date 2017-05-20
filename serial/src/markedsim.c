4f79fc3c803633cc4664ad53ac1ba56c27f66531
Nils M <nils.meier@studium.uni-hamburg.de>
2017-05-20 17:29:30 +0200

    testdata

--- serial/src/markedsim.c
+++ serial/src/markedsim.c
@@ -1,6 +1,19 @@
 #include "markedsim.h"
 
-struct field*** createmarked(int x, int y, int floor_count){
+struct field***
+
+int getfield(char* line, int num)
+{
+    const char* tok;
+    for (tok = strtok(line, ","); tok && *tok; tok = strtok(NULL, ";\n"))
+    {
+        if (!--num)
+            return atoi(tok);
+    }
+    return NULL;
+}
+
+createmarked(int x, int y, int floor_count){
 	struct  field *** marked = malloc(floor_count * sizeof(struct field**)); 
 	for(int i = 0; i < floor_count; i++){
 		struct  field** floor = malloc(y * sizeof(struct field*));
@@ -12,7 +25,26 @@ struct field*** createmarked(int x, int y, int floor_count){
 	return marked;
 }
 
-void freemarked(struct field*** marked, int y, int floor_count){
+struct field***
+import_marked(char* path, int* x, int* y, int* floor_count){
+	FILE *file = fopen(path, "r");
+	char line[255];
+	fgets(line, sizeof(line), file);
+	*x = getfield(char* line, 0);
+	*y = getfield(char* line, 1);
+	*floor_count = getfield(char* line, 3);
+	
+	//check if file exists
+	if (fp == NULL) exit(EXIT_FAILURE);
+	
+	struct field*** marked = createmarked(*x, *y, *floor_count);
+	
+	fclose(file);
+return marked
+}
+
+void 
+freemarked(struct field*** marked, int y, int floor_count){
 	for(int o = 0; o < floor_count; o++ ){
 		for(int i = 0; i < y; i++){
 			free(marked[o][i]);
@@ -22,22 +54,12 @@ void freemarked(struct field*** marked, int y, int floor_count){
 	free(marked);
 }
 
-
-void import_marked(char* path){
-
-}
-
 int
-main(){
+main(int argc, char *argv[]){
+if (arc < 1) exit(EXIT_FAILURE);
 
-
-struct field*** marked = createmarked(x, y, floor_count);
-
-
-
-
-
-
+int x, y, floor_count;	
+import_marked(argv, &x, &y, &floor_count);
 
 freemarked(marked, y, floor_count);
 }
